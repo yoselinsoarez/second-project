762560e1ccebfd6f06be94882bfff182ff9f09ea
yoselinsoarez <yoselinsoarez93@gmail.com>
2023-02-01 15:03:18 -0300

    css changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../stylesheets/AboutUS.css';
 
 function AboutUs() {
 return (
-    <div>
+    <div className='about-container'>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
     </div>
 )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../stylesheets/Home.css';
 
 
 function Home() {
 return (
-    <div>
+    <div className='home-container'>
         <h1>This is an adoption page</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
         
